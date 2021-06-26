1d3b555ef5ae57ed3d49a2a2176486eb9004e31e
Tian Xiao <tianxiaos1202@gmail.com>
2021-06-26 22:28:39 +0800

    Update

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from random import *
 # initial state
 DATE = 2100
 DATE_INC = 20
-TEMPERATURE = 22
+TEMPERATURE = 20 #previously 22
 POPULATION = 1000 # maximum: 100000
 
 POP_INC = 0.2
-TEMP_INC = 0.16
+TEMP_INC = 0.15 #previously 0.16
 
 WATER = 350000000000 # m3
 FOREST = 350000000 # m2
@@ -21,8 +21,8 @@ RENEWABLE = 0 # out of 10
 LIVING_MODE = 0 # 0, 1, 2
 
 # energy conversion
-ENERGY_PER_TREE = 100
-ENERGY_PER_FOSSIL_FUEL = 10000000
+ENERGY_PER_TREE = 10
+ENERGY_PER_FOSSIL_FUEL = 100000000
 
 # resource used
 ENERGY_USAGE = [10000000 * i for i in range(11)]
@@ -36,12 +36,12 @@ def cal_usage(population, energy_per_capita):
 
 def cal_energy_per_capita(INDUSTRY, LIVING_MODE):
     # linear
-    energy_per_capita = INDUSTRY * 10000 * 0.5 * (LIVING_MODE + 1)
+    energy_per_capita = INDUSTRY * 10000 * 0.5 * (LIVING_MODE+1)*2
 
     return energy_per_capita
 
-def cal_pop_inc(pop_inc, industry, healthcare, agriculture, temperature):
-    curr_pop_inc = pop_inc + 0.01 * industry + 0.01 * healthcare + 0.01 * agriculture - 0.15 * (temperature - TEMPERATURE)
+def cal_pop_inc(pop_inc, industry, healthcare, agriculture, temperature, living_mode):
+    curr_pop_inc = pop_inc + 0.03 * industry + 0.03 * healthcare + 0.03 * agriculture - 0.015 * (temperature - TEMPERATURE) + (living_mode/2 - 0.5)
 
     return curr_pop_inc
 
@@ -71,6 +71,7 @@ def main():
         date += DATE_INC
         print("current year: " + str(date) + "\n")
         print("current population: " + str(population) + "\n")
+        print("current temperature: " + str(temperature) + "\n")
         print(f"current resources: \nwater - {water}; forest - {forest}; fossil fuel - {fossil_fuel * 100}%;")
         print(f"current technology: \nindustry - {industry}; healthcare - {healthcare}; agriculture - {agriculture}; renewable - {renewable}\n")
 
@@ -102,15 +103,18 @@ def main():
 
         energy_consumed = energy_usage - ENERGY_GENERATED[renewable] if energy_usage > ENERGY_GENERATED[renewable] else 0
 
-        pop_inc = cal_pop_inc(pop_inc, industry, healthcare, agriculture, temperature)
+        pop_inc = cal_pop_inc(pop_inc, industry, healthcare, agriculture, temperature, curr_living_mode)
         population = int(population * (1 + pop_inc))
-
+        
+        temp_inc -= industry/1000
+        
         if fossil_fuel != 0 and forest != 0:
 
+            #print("energy consumed is: " + str(energy_consumed))
             energy_consumed_by_fossil_fuel = energy_consumed * fossil_fuel * ENERGY_PER_FOSSIL_FUEL / (fossil_fuel * ENERGY_PER_FOSSIL_FUEL + forest * ENERGY_PER_TREE)
 
             fossil_fuel -= energy_consumed_by_fossil_fuel / ENERGY_PER_FOSSIL_FUEL
-
+            temp_inc = TEMP_INC + round(energy_consumed_by_fossil_fuel /10000000000,4) ###
             
 
             if fossil_fuel < 0:
@@ -129,16 +133,22 @@ def main():
         if water < 0:
             water = 0
             population = -1
-
+        
         # disease
         disease = randint(1,11)
-        disease_kill = max((disease - healthcare) * 100, int((disease - healthcare) / 10 * population))
+        disease_kill = max((disease - healthcare) * 100, int((disease - healthcare) / 10 * population))//2
         if disease_kill < 0:
             disease_kill = 0
         elif disease_kill > population:
             disease_kill = population
         population -= disease_kill
-        print(f"You experience a disease of severity level {disease} and {disease_kill} people died.")
+        print(f"You experienced a disease of severity level {disease} and {disease_kill} people died.")
+        temperature = round(temperature * (1 + temp_inc), 4)
+        if temperature > 150:
+            population = -1
+            print("Human beings are yet unable to live under a temperature above 150 celcius degress :(")
+    
+        
 
     print(f"Human die out. You have survived for {date - DATE} years.");
 
